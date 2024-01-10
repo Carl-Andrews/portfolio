eaf274bd8fb0b762a02188d8f8fc80801717cd5c
Carl <carlandrews.k@gmail.com>
2024-01-10 01:53:06 +0100

    first commit

---
+++ js/jscript.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.carousel').slick({
+      autoplay: false,
+      autoplaySpeed: 4000, 
+      dots: false, 
+      infinite: true, 
+      speed: 1000, 
+      slidesToShow: 1, 
+      slidesToScroll: 1 
+    });
+  });
